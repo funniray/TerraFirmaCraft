c58e91ed165c4665066a7a1625aa84f23d63d6f5
funniray <funniray10@gmail.com>
2024-11-22 01:31:51 +0000

    Revert "Implement item stack capability sync in a way that is compatible with the vanilla protocol, fixes #2753"
    This reverts commit 9d86cebb

--- src/main/java/net/dries007/tfc/common/capabilities/ItemStackCapabilitySync.java
+++ src/main/java/net/dries007/tfc/common/capabilities/ItemStackCapabilitySync.java
@@ -7,11 +7,10 @@
 package net.dries007.tfc.common.capabilities;
 
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.INBTSerializable;
-import org.jetbrains.annotations.Nullable;
 
 import net.dries007.tfc.common.capabilities.food.FoodCapability;
 import net.dries007.tfc.common.capabilities.heat.HeatCapability;
@@ -23,16 +22,14 @@ import net.dries007.tfc.common.capabilities.heat.HeatCapability;
  * This is <strong>impossible</strong> with {@link HeatCapability} and {@link FoodCapability} as their serialization can change without intervention, so it cannot be saved on modification.
  * <p>
  * A note on why {@link ItemStack#getShareTag()} does not work for this purpose:
- * <ul>
- *     <li>It needs to be overriden for items (which means it works for none of our event attached capabilities)</li>
- *     <li>In the cases where we could use it, it's not needed (because we just stick to using the stack tag which should ALWAYS be synced.)</li>
- *     <li>It's still ineffective in the case of the creative inventory.</li>
- * </ul>
+ * - It needs to be overriden for items (which means it works for none of our event attached capabilities)
+ * - In the cases where we could use it, it's not needed (because we just stick to using the stack tag which should ALWAYS be synced.)
+ * - It's still ineffective in the case of the creative inventory.
+ * <p>
  * So, in order to solve both problems (S -> C sync, and creative inventory C -> S sync):
- * <ul>
- *     <li>We patch the encode item stack to use this method, to sync non-stack-tag capabilities</li>
- *     <li>All other capabilities use the stack tag to avoid sync concerns.</li>
- * </ul>
+ * - We patch the encode item stack to use this method, to sync non-stack-tag capabilities
+ * - All other capabilities use the stack tag to avoid sync concerns.
+ * <p>
  * Finally, in order to avoid issues caused by other mods due to incorrectly synced item stacks (see <a href="https://github.com/TerraFirmaCraft/TerraFirmaCraft/issues/2198">TerraFirmaCraft#2198</a>), we need to write and read this data in an as unconditional method as possible.
  * This means we cannot check for empty stacks, or those that do not have a capability. In the best case, we write an additional +1 bytes per item stack (a typical item stack has ~4-6 bytes default). This is about as least-cost that we can make it (in the worst case, we write 1 + two nbt tags).
  * <p>
@@ -40,86 +37,54 @@ import net.dries007.tfc.common.capabilities.heat.HeatCapability;
  */
 public final class ItemStackCapabilitySync
 {
-    private static final String FOOD_ID = "tfc:food";
-    private static final String HEAT_ID = "tfc:heat";
-    private static final String EMPTY_ID = "tfc:empty";
-
     public static boolean hasSyncableCapability(ItemStack stack)
     {
         return stack.getCapability(FoodCapability.NETWORK_CAPABILITY).isPresent() || stack.getCapability(HeatCapability.NETWORK_CAPABILITY).isPresent();
     }
 
-    @Nullable
     @SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
-    public static CompoundTag writeToNetwork(ItemStack stack, @Nullable CompoundTag originalTag)
+    public static void writeToNetwork(ItemStack stack, FriendlyByteBuf buffer)
     {
-        // getCapability().resolve() might be called on an uninitialized stack here, which actually involves a mutation to the stack, as
-        // capabilities will be initialized for the first time. This initialization is not threadsafe, in general. In particular, during
-        // deserialization/serialization of food capabilities, food traits are mutated and iterated through a list.
-        //
-        // While this is a weird construct, we should be able to synchronize on the item stack itself - this will prevent any stacks
-        // from having getCapability invoked and resolved by two threads at once. Different stacks should be fully safe to execute independently.
-        //
-        // Additionally, we write the extra capability data as part of the original stack tag. This is to preserve compatibility between vanilla
-        // clients (see TerraFirmaCraft#2753) by only using the vanilla network protocol. This means we have to do some finicky handling to make
-        // sure we don't modify any base cases that may occur.
+        // getCapability().resolve() might be called on an uninitialized stack here, which actually involves a mutation to the stack, as capabilities will be initialized for the first time
+        // This initialization is not threadsafe, in general. In particular, during deserialization/serialization of food capabilities, food traits are mutated and iterated through a list.
+        // While this is a weird construct, we should be able to synchronize on the item stack itself - this will prevent any stacks from having getCapability invoked and resolved by two threads at once. Different stacks should be fully safe to execute independently.
         synchronized (stack)
         {
             if (hasSyncableCapability(stack))
             {
-                final CompoundTag writableTag = originalTag == null
-                    ? new CompoundTag()
-                    : originalTag.copy(); // Must avoid mutating the original tag
-
-                // Note that the original tag might be empty - if it was, then we flag it here
-                if (originalTag != null && originalTag.isEmpty())
-                {
-                    writableTag.putBoolean(EMPTY_ID, true);
-                }
-                writeToNetwork(FOOD_ID, FoodCapability.NETWORK_CAPABILITY, stack, writableTag);
-                writeToNetwork(HEAT_ID, HeatCapability.NETWORK_CAPABILITY, stack, writableTag);
-                return writableTag;
+                buffer.writeBoolean(true);
+                writeToNetwork(FoodCapability.NETWORK_CAPABILITY, stack, buffer);
+                writeToNetwork(HeatCapability.NETWORK_CAPABILITY, stack, buffer);
+            }
+            else
+            {
+                buffer.writeBoolean(false);
             }
         }
-        return originalTag;
     }
 
-    @Nullable
-    public static CompoundTag readFromNetwork(ItemStack stack, @Nullable CompoundTag tag)
+    public static void readFromNetwork(ItemStack stack, FriendlyByteBuf buffer)
     {
+        if (buffer.readBoolean())
+        {
+            readFromNetwork(FoodCapability.NETWORK_CAPABILITY, stack, buffer);
+            readFromNetwork(HeatCapability.NETWORK_CAPABILITY, stack, buffer);
+        }
+    }
+
+    private static void writeToNetwork(Capability<? extends INBTSerializable<CompoundTag>> capability, ItemStack stack, FriendlyByteBuf buffer)
+    {
+        buffer.writeNbt(stack.getCapability(capability)
+            .map(INBTSerializable::serializeNBT)
+            .orElse(null));
+    }
+
+    private static void readFromNetwork(Capability<? extends INBTSerializable<CompoundTag>> capability, ItemStack stack, FriendlyByteBuf buffer)
+    {
+        final CompoundTag tag = buffer.readNbt();
         if (tag != null)
         {
-            // Always read, if we can. This removes the NBT from the network if we added it
-            boolean modified = false;
-            modified |= readFromNetwork(FOOD_ID, FoodCapability.NETWORK_CAPABILITY, stack, tag);
-            modified |= readFromNetwork(HEAT_ID, HeatCapability.NETWORK_CAPABILITY, stack, tag);
-            if (tag.isEmpty() && modified)
-            {
-                // In this instance, the tag was modified by us, and we removed all the content that is contained.
-                // Check for the presence of an empty flag, which would indicate that the original tag was still present (but empty)
-                final boolean wasEmptyButPresent = tag.getBoolean(EMPTY_ID);
-                tag.remove(EMPTY_ID);
-                if (!wasEmptyButPresent) return null;
-            }
+            stack.getCapability(capability).ifPresent(cap -> cap.deserializeNBT(tag));
         }
-        return tag;
-    }
-
-    private static void writeToNetwork(String networkId, Capability<? extends INBTSerializable<CompoundTag>> capability, ItemStack stack, CompoundTag rootTag)
-    {
-        stack.getCapability(capability)
-            .map(INBTSerializable::serializeNBT)
-            .ifPresent(tag -> rootTag.put(networkId, tag));
-    }
-
-    private static boolean readFromNetwork(String networkId, Capability<? extends INBTSerializable<CompoundTag>> capability, ItemStack stack, CompoundTag tag)
-    {
-        if (tag.contains(networkId, Tag.TAG_COMPOUND))
-        {
-            stack.getCapability(capability).ifPresent(cap -> cap.deserializeNBT(tag.getCompound(networkId)));
-            tag.remove(networkId);
-            return true;
-        }
-        return false;
     }
 }
